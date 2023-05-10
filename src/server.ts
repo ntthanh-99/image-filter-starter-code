c5e59506be77bf0bf2b2e9464c3ad35aeee16543
ThanhNT <63948294+ntthanh-99@users.noreply.github.com>
2023-05-10 23:10:02 +0700

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,54 @@
+import express, { Router, Request, Response } from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import { join } from "bluebird";
+
+(async () => {
+  // Init the Express application
+  const app = express();
+  const fs = require("fs");
+  const path = require("path");
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  let url = "src/util/tmp";
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  app.get("/filteredimage/", async (req: Request, res: Response) => {
+    //    1. validate the image_url query
+    //    image_url: URL of a publicly accessible image
+    let { image_url } = req.query;
+    if (!image_url) {
+      return res.status(400).send(`image_url is required`);
+    }
+
+    //    2. call filterImageFromURL(image_url) to filter the image
+    const image = await filterImageFromURL(image_url);
+
+    //    3. send the resulting file in the response
+    res.sendFile(image, function (err) {
+      //    4. deletes any files on the server on finish of the response
+      const files = fs.readdirSync(url);
+      const absolutePaths: Array<string> = [];
+      files.forEach((file: File) => {
+        absolutePaths.push(path.join(url, file));
+      });
+      deleteLocalFiles(absolutePaths);
+    });
+  });
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req: Request, res: Response) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
